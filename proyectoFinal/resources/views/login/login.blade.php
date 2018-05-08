87d3f1c081469ba9419c65da8ac1ad7af988c0c7
aitor <edtalopez@gmail.com>
2018-05-08 13:49:09 +0200

    creamos formulario para el login

--- proyectoFinal/resources/views/login/login.blade.php
+++ proyectoFinal/resources/views/login/login.blade.php
@@ -8,7 +8,45 @@
 
 @section('content')
 
-    <form class="form-singin">
+    <div class="container mt-5 mb-5">
+        <div class="row col-md-offset-2">
+            <div class="col col-6 col-md-6 offset-md-3 pt-5 pb-5">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h4 class="panel-title text-center text-capitalize">Iniciar Sesión</h4></div>
+                    <div class="panel-body pt-2 pb-2 pr-2-pl-2">
+                        {!! Form::open() !!}
+                        {{csrf_field()}}
+                        <div class="form-group row">
+                            {!! Form::label('usuario','Usuario',['class' => 'col-sm-2 col-form-label']) !!}
+                            <div class="col-sm-10">
+                                {!! Form::text('usuario', null, ['class' => 'form-control', 'placeholder' => 'Usuario']) !!}
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            {!! Form::label('password','Contraseña',['class' => 'col-sm-2 col-form-label']) !!}
+                            <div class="col-sm-10">
+                                {!! Form::text('password', null, ['class' => 'form-control', 'placeholder' => 'Contraseña']) !!}
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-10 mt-3">
+                                {!! Form::submit('Iniciar Sesión' , ['class' => 'btn btn-dark']) !!}
+                                {{
+                                        Form::macro('botonCancelar', function(){
+                                            return '<a role="button" class=" btn btn-dark" href="'.route('inicio').'">Cancelar</a>';
+                                        })
+                                    }}
+                                {!! Form::botonCancelar() !!}
+                                <span>No estas registrado? <a href="{{route('registro')}}">Registrate!</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--<form class="form-singin">
         <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="inputEmail" class="sr-only">Email address</label>
@@ -21,6 +59,6 @@
             </label>
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>
+    </form>-->
 
 @endsection
